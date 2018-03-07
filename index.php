39892aae74a3094fd874fdee5d58efa84188c682
jmfs <jmfs@companymobile.dk>
2018-03-07 15:13:54 +0100

    com

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
 
-<body><div class="content">Site contents here. Test</div>
+<body><div class="content">Site contents here. Test update me.</div>
 </body>
 </html>
 </html>
