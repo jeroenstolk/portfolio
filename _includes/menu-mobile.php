35973c415dfafaf808ce63b90d5301c6fecd7cac
portfolio <info@jeroen.dk>
2018-03-11 22:29:34 +0100

    com

--- _includes/menu-mobile.php
+++ _includes/menu-mobile.php
@@ -1,1 +1,114 @@
-<?php echo "menu mobile"; ?>
+<?php
+echo '<nav class="wrapper">
+<input id="menu-toggle" type="checkbox">
+<label for="menu-toggle" class="label-toggle"></label>
+
+<ul id="menu-mobile">
+
+<li class="prime">
+<a href="#" id="forhandlere">Forhandlere</a>
+</li>
+<li class="sub forhandlere" style="display:none;">
+<ul>
+<li><a href="/Reseller/Create">Opret forhandler</a></li>
+<li><a href="/Reseller">Vis forhandlere</a></li>
+<li><a href="/Reseller/Search">Søg forhandlere</a></li>
+</ul>
+</li>
+
+<li class="prime">
+<a href="#" id="shortcodes">Shortcodes</a>
+</li>
+<li class="sub shortcodes" style="display:none;">
+<ul>
+<li><a href="/Shortcode/Create">Opret Shortcode</a></li>
+<li><a href="/Shortcode">Vis Shortcodes</a></li>
+<li><a href="/Shortcode/Search">Søg Shortcode</a></li>
+</ul>
+</li>
+
+<li class="prime">
+<a href="#" id="kommunikation">Kommunikation</a>
+</li>
+<li class="sub kommunikation" style="display:none;">
+<ul>
+<li><a href="/Communication/CreateTargetGroup">Opret Målgruppe</a></li>
+<li><a href="/Communication/ShowTargetGroup">Vis Målgrupper</a></li>
+<li><a href="/Communication/CreateNewsSMS">Opret Nyheds SMS</a></li>
+<li><a href="/Communication/ShowNewsSMS">Vis Nyheds SMS</a></li>
+<li><a href="/Communication/SMSAwaitingSend">Afventende Afsendelse</a></li>
+<li><a href="/Communication/SMSCompletedSend">Udførte Afsendelse</a></li>
+</ul>
+</li>
+
+<li class="prime">
+<a href="#" id="keywords">Keywords</a>
+</li>
+<li class="sub keywords" style="display:none;">
+<ul>
+<li><a href="/Keyword/Create">Opret Keyword</a></li>
+<li><a href="/Keyword">Vis Keywords</a></li>
+<li><a href="/Keyword/Search">Søg Keyword</a></li>
+</ul>
+</li>
+
+<li class="prime">
+<a href="#" id="abonnement-priser">Abonnement/priser</a>
+</li>
+<li class="sub abonnement-priser" style="display:none;">
+<ul>
+<li><a href="/Subscription/SMSPrice">SMS priser</a></li>
+<li><a href="/Subscription/SMSPriceInternational">SMS priser - udland</a></li>
+<li><a href="/Subscription/Share">Share</a></li>
+<li class="hidden"><a href="/Subscription/Subscriptions">Abonnementer</a></li>
+<li><a href="/Subscription/CreateSubscription">Opret Abonnement</a></li>
+</ul>
+</li>
+
+<li class="prime">
+<a href="#" id="statistik">Statistik</a>
+</li>
+<li class="sub statistik" style="display:none;">
+<ul>
+<li><a href="/Statistic">Overblik</a></li>
+<li><a href="/Statistic/SMS">SMS</a></li>
+<li><a href="/Statistic/Economics">Økonomi</a></li>
+<li><a href="/Statistic/RawData">Rådata csv</a></li>
+</ul>
+</li>
+
+<li class="prime">
+<a href="#" id="fakturering">Fakturering</a>
+</li>
+<li class="sub fakturering" style="display:none;">
+<ul>
+<li><a href="/Billing/Payment">Indbetalinger</a></li>
+<li><a href="/Billing/Payout">Udbetalinger</a></li>
+</ul>
+</li>
+
+<li class="prime">
+<a href="#" id="diverse">Diverse</a>
+</li>
+<li class="sub diverse" style="display:none;">
+<ul>
+<li><a href="/Miscellaneous/UserAdministration">Brugeradministration</a></li>
+<li><a href="/Miscellaneous/IpRestrictions">Ip Restrictions</a></li>
+<li><a href="/Miscellaneous/TextSettings">Tekstopsætning</a></li>
+<li><a href="/Miscellaneous/DocumentAdministration">Download administrator</a></li>
+</ul>
+</li>a
+
+<li class="prime">
+<a href="#" id="sms_log">SMS Log</a>
+</li>
+<li class="sub sms_log" style="display:none;">
+<ul>
+<li><a href="/SMSLog/SmsReceived">Modtaget</a></li>
+<li><a href="/SMSLog/SmsSent">Sendt</a></li>
+</ul>
+</li>
+<li><a href="/Account/LogOff">Log af</a></li>
+</ul>
+</nav>';
+?>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=500, initial-scale=1">
 	<title>Portfolio</title>
 	<link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
@@ -10,7 +11,7 @@
 <body>
 	<?php include "_includes/menu.php"; ?>
 	<div class="content">
-		<h1>Great! You're here!!</h1>
+		<h1>Great! You're here!!!</h1>
 		<h2>Hi there and welcome to my portfolio</h2>
 
 	
@@ -25,10 +26,71 @@
 		</p>
 	
 	<?php
-	$section = '<div class="section">section</div>';
+	$table = '<table class="table ">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Status</th>
+                        <th>Type</th>
+                        <th>Keywords</th>
+                        <th>Last updated</th>
+                        <th>Last 30 days</th>
+                        <th class="table-actions"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                        <tr>
+        
+<td>Videresendelse til intranet</td>
+<td class="table-status">
+    <input value="3" class="jsAId" type="hidden">
+    <input value="1" class="jsTId" type="hidden">
+        <div class="status-icon-active switch"></div>
+        <button class="button-start-stop jsActionDeactivate">stop</button>
+</td>
+<td>Forward to URL</td>
+<td style="text-align:left;">CME </td>
+<td>10/19/2017 2:26:49 PM</td>
+<td>0</td>
+<td>
+    <button class="status-icon-edit right jsActionEdit jsLaunchModal"></button>
+    <button class="status-icon-delete jsActionDelete jsLaunchModal"></button>
+</td>
+
+    </tr>
+    <tr>
+        
+<td>Fast svar</td>
+<td class="table-status">
+    <input value="4" class="jsAId" type="hidden">
+    <input value="2" class="jsTId" type="hidden">
+        <div class="status-icon-done switch"></div>
+        <button class="button-start-stop jsActionActivate">start</button>
+</td>
+<td>Automatic reply</td>
+<td style="text-align:left;">CME </td>
+<td>10/20/2017 1:54:21 PM</td>
+<td>0</td>
+<td>
+    <button class="status-icon-edit right jsActionEdit jsLaunchModal"></button>
+    <button class="status-icon-delete jsActionDelete jsLaunchModal"></button>
+</td>
+
+    </tr>
+
+                </tbody>
+            </table>';
+		$section = '<div class="section">section</div>';
+	$section2 = '<div class="section">'.$table.'</div>';
+
+	
 	for ( $i = 1; $i <= 5; $i++ ) {
-		echo $section;
-	}
+		if ($i == 3){
+			echo $section2;
+		} else {
+			echo $section;
+		};
+	};
 	?>
 
 	</div>
